2bf53b468c1f0e615e6eb32bb5fdc19ac7c421dd
1704822 <1704822@DASOE67278>
2018-07-31 21:26:35 -0300

    Interações com usuario

---
+++ Aulas/Aula3107_Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula3107_Console
+{
+    class Program
+    {
+
+        public int Id { get; set; }
+        static void Main string[] args)
+        
+        {
+            Console.WriteLine("Hello Wordl!");
+            Console.ReadKey();
+           
+            Console.WriteLine("Digite um numero");
+            Id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Voce digitou: " + id);
+            Console.ReadKey();
+        }
+    }
+}
