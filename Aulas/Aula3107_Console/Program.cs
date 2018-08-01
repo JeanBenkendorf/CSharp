1202fc8c2395e048781c8a4fc46c2eac215db949
1704822 <1704822@DASOE67278>
2018-07-31 22:05:47 -0300

    Conclusao da aula

--- Aulas/Aula3107_Console/Program.cs
+++ Aulas/Aula3107_Console/Program.cs
@@ -8,17 +8,85 @@ namespace Aula3107_Console
 {
     class Program
     {
-
         public static int Id { get; set; }
-        static void Main (string[] args)
-        
+        public static int numero1 { get; set; }
+        public static int numero2 { get; set; }
+        public static int opcao { get; set; }
+
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello Wordl!");
+            Console.WriteLine("Ola\n");
             Console.ReadKey();
-           
-            Console.WriteLine("Digite um numero");
+
+
+            Console.WriteLine("\n\nInforme o primeiro número(outra operação): ");
             Id = int.Parse(Console.ReadLine());
-            Console.WriteLine("Voce digitou: " + Id);
+            ImprimirNumeroDigitado(Id);
+            Console.ReadKey();
+            // novo exercício
+
+            Console.WriteLine("\nFavor, informar o primeiro número: ");
+            numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nFavor, informar o segundo número:");
+            numero2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual operação deseja fazer?\n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão");
+            opcao = int.Parse(Console.ReadLine());
+
+            if (opcao == 1)
+            {
+                soma(numero1, numero2);
+            }
+            else
+            {
+                if (opcao == 2)
+                {
+                    sub(numero1, numero2);
+                }
+                else
+                {
+                    if (opcao == 3)
+                    {
+                        multipicacao(numero1, numero2);
+                    }
+                    else
+                    {
+                        if (opcao == 4)
+                        {
+                            divisao(numero1, numero2);
+                        }
+                    }
+                }
+            }
+
+
+
+
+        }
+        static void ImprimirNumeroDigitado(int num)
+        {
+            Console.WriteLine("você digitou: " + num);
+        }
+
+        static void soma(int num1, int num2)
+        {
+            Console.WriteLine("A soma dos números é: " + (num1 + num2));
+            Console.ReadKey();
+        }
+        static void sub(int num1, int num2)
+        {
+            Console.WriteLine("A subtração dos números é: " + (num1 - num2));
+            Console.ReadKey();
+        }
+        static void multipicacao(int num1, int num2)
+        {
+            Console.WriteLine("A multiplicação dos números é: " + (num1 * num2));
+            Console.ReadKey();
+        }
+
+        static void divisao(int num1, int num2)
+        {
+            Console.WriteLine("A divisão dos números é: " + (num1 / num2));
             Console.ReadKey();
         }
     }
