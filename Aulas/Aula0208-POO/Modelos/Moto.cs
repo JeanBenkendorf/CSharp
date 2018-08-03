4222a796a5f9116c872f0e40a115096f532ca6b4
JeanBenkenorf <jcarlolopes@yahoo.com.br>
2018-08-02 21:57:12 -0300

    Projeto em andamento

---
+++ Aulas/Aula0208-POO/Modelos/Moto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula0208_POO.Modelos
+{
+    class Moto : Veiculo, IMotorizado
+    {
+        public int Velocidade { get ; set ; }
+
+        public void Frenar()
+        {
+            Console.WriteLine("Moto: Acelerando pela manopla.");
+            Velocidade += 10;
+        }
+
+        void IMotorizado.Acelerar()
+        {
+            Console.WriteLine("Descendo a ladeira...");
+            Velocidade += 20;
+        }
+    }
+}
---
+++ Aulas/Aula0208-POO/Program.cs
@@ -0,0 +1,36 @@
+﻿using Aula0208_POO.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula0208_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro car = new Carro();
+            car.Modelo = "Ferrari";
+            car.Acelerar();
+            ExecutarTesteMotor(car);
+            Console.ReadKey();
+
+            Console.WriteLine("Velocidade: " + car.Velocidade);
+            Console.ReadKey();
+        }
+
+        static void ExecutarTesteMotor(IMotorizado vec)
+        {
+            Console.WriteLine("Velocidade Atual" + vec.Velocidade );
+            vec.Acelerar();
+            vec.Acelerar();
+            vec.Acelerar();
+            Console.WriteLine("Velocidade Atual" + vec.Velocidade);
+            vec.Frenar();
+            vec.Frenar();
+            Console.WriteLine("Velocidade Atual" + vec.Velocidade);
+        }
+    }
+}
