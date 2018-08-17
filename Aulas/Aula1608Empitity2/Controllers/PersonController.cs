1a8d9f41a328a84dc8bda0e0494aed39618fb8ba
Jean Benkendorf Jean Benkendorf <jean.benkendorf@outlook.com>
2018-08-16 21:58:50 -0300

    etapa final

---
+++ Aulas/Aula1608Empitity2/Controllers/PersonController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1608Empitity2.Controllers
+{
+    class PersonController
+    {
+
+        void inserir(Person p)
+        {
+            AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+
+            contexto.Person.Add(p);
+            contexto.SaveChanges();
+
+        }
+
+        List<Person> ListarTodoPerson()
+        {
+            AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+            return contexto.Person.ToList();
+
+        }
+
+
+    }
+}
