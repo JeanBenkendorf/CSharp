05d7f82075026f9285c5ffb5866111929171c623
Jean Benkendorf Jean Benkendorf <jean.benkendorf@outlook.com>
2018-08-21 21:42:34 -0300

    cirando estrutura da classe produto

---
+++ Aulas/Aula2108_CodeFirst/Modelos/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula2108_CodeFirst.Modelos
+{
+    class Produto
+    {
+        public int ProdutoId { get; set; }
+        public String Nome { get; set; }
+        public String Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public decimal Imposto { get; set; }
+
+        //Relacionamento Produto <--> Categoria
+        public int CategoriaId { get; set; }
+        //Propriedade Navegação
+        public virtual Categoria _Categoria { get; set; }
+
+
+    }
+}
