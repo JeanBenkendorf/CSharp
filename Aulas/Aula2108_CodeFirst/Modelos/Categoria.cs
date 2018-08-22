f687941626127676b647ab3b2572cd76a61a5af5
Jean Benkendorf Jean Benkendorf <jean.benkendorf@outlook.com>
2018-08-21 21:52:48 -0300

    Relacionamento de produto e categoria

--- Aulas/Aula2108_CodeFirst/Modelos/Categoria.cs
+++ Aulas/Aula2108_CodeFirst/Modelos/Categoria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,14 @@ namespace Aula2108_CodeFirst.Modelos
 {
     class Categoria
     {
+        [Key]
         public int CategoriaId { get; set; }
+        [Required];
+        [StringLength(30)];
         public int Nome { get; set; }
         public int Descricao { get; set; }
+
+        //Relacionamento Categoria <--> Produto
+        public virtual ICollection<Produto> _Produtos { get; set; }
     }
 }
